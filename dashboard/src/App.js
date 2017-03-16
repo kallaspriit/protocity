3c567da44b2f212b654650081cbb0ddcbe855b75
Taavi Sangel <taavi@stagnationlab.com>
2017-03-16 18:31:22 +0200

    stated with train view and truck view

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import HomeView from './views/home/HomeView';
+import TrainView from './views/train/TrainView';
+import TruckView from './views/truck/TruckView';
 
 export default () => (
 	<div className="app">
 		<Route exact path="/" component={HomeView} />
+		<Route exact path="/train" component={TrainView} />
+		<Route exact path="/truck" component={TruckView} />
 	</div>
 );
---
+++ dashboard/src/views/truck/TruckView.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './truck-view.scss';
+
+const TruckView = () => (
+	<div className="truck-view">
+		truck view
+	</div>
+);
+
+export default TruckView;
