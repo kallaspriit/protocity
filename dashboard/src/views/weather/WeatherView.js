f59850a4c24a42f730af5ace5eee066c28433bab
Priit Kallas <kallaspriit@gmail.com>
2017-03-24 10:58:38 +0200

    Updated weather.

--- dashboard/src/views/weather/WeatherView.js
+++ dashboard/src/views/weather/WeatherView.js
@@ -61,7 +61,7 @@ export const WeatherView = ({ weather, lighting }) => (
 			<div className="data">
 				<h2 className="data__title">Detect light level from weather station</h2>
 				<div className="data__chart data__chart--large">
-					<p className="data__value">{lighting.detectedLightLevel}</p>
+					<p className="data__value">{weather.lightLevel}</p>
 					<p className="data__measure">lux</p>
 				</div>
 			</div>
@@ -69,7 +69,7 @@ export const WeatherView = ({ weather, lighting }) => (
 			<div className="data">
 				<h2 className="data__title">Light level / energy cost in kWh</h2>
 				<div className="data__chart data__chart--large">
-					<p className="data__value">{(Math.round(62 * weather.lightLevel * 10) / 10)}</p>
+					<p className="data__value">{(Math.round(62 * lighting.outputLightLevel * 10) / 10)}</p>
 					<p className="data__measure">kW</p>
 				</div>
 			</div>
